329b9bb838b32d46672b9bb013be026ae48a1e58
keeniGithub <maratkanovartem89@mail.ru>
2026-08-04 18:34:37 +0300

    add proxy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/proxy/:path*",
+        destination: "http://kenyhost.duckdns.org:8006/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
